8c49836f94efbac186bdd86e793cef07ae20562c
Vladimir Balko <vbalko@gmail.com>
2020-05-25 18:08:30 +0000

    D3 example text rendering
    Signed-off-by: Vladimir Balko <vbalko@gmail.com>

--- d3/app.js
+++ d3/app.js
@@ -1,6 +1,13 @@
+const DUMMY_DATA =  [
+    { id: 'd1', value: 10, region: 'USA'},
+    { id: 'd2', value: 12, region: 'Izrael'},
+    { id: 'd3', value: 15, region: 'UK'},
+    { id: 'd4', value: 13, region: 'NL'},
+];
+
 d3.select('div')
     .selectAll('p')
-    .data([1, 2, 3])
+    .data(DUMMY_DATA)
     .enter()
     .append('p')
-    .text(data => data);
+    .text(data => data.region);
